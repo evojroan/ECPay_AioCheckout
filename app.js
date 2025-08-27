79dd4ebcced69bf1c70953e64861f2055e0363ac
R0An <roan6903@gmail.com>
2025-08-27 09:59:43 +0800

    改為伺服器產生連結，點選連結後直接進入全方位金流付款頁面

--- app.js
+++ app.js
@@ -1,31 +1,34 @@
-//綠界全方位金流技術文件： https://developers.ecpay.com.tw/?p=2509
+const express = require("express");
+const app = express();
 
 ////////////////////////改以下參數即可////////////////////////
 //一、選擇帳號，是否為測試環境
 const MerchantID = "3002607"; //必填
 const HashKey = "pwFHCqoQZGmho4w6"; //3002607
 const HashIV = "EkRm7iFT261dpevs"; //3002607
-let isStage = true; // 測試環境： true；正式環境：false
+const isStage = true; // 測試環境： true；正式環境：false
+const timestamp = Date.now();
 
 //二、輸入參數
-let parameters={
-
-  'TotalAmount':1000,
-  'TradeDesc':"Test Order",
-  'ItemName':'Test Product',
-  'ReturnURL' :"https://yourwebsite.com/return",
-  'ChoosePayment' : "ATM",
-  'ChooseSubPayment':"PANHSIN"
-  
+function getParameters() {
+  return {
+    MerchantID: MerchantID,
+    MerchantTradeNo: MerchantTradeNo,
+    MerchantTradeDate: MerchantTradeDate,
+    PaymentType: "aio",
+    TotalAmount: 100,
+    TradeDesc: "shopping",
+    ItemName: "Item",
+    ReturnURL: "https://www.ecpay.com.tw/",
+    ChoosePayment: "ALL",
+  };
 }
-
 
 ////////////////////////以下參數不用改////////////////////////
 const stage = isStage ? "-stage" : "";
-const algorithm = "sha256";
-const digest = "hex";
-const APIURL = `https://payment${stage}.ecpay.com.tw//Cashier/AioCheckOut/V5`;
-const MerchantTradeNo = `od${new Date().getFullYear()}${(
+const APIURL = `https://payment${stage}.ecpay.com.tw/Cashier/AioCheckOut/V5`;
+
+const MerchantTradeNo = `ecpay${new Date().getFullYear()}${(
   new Date().getMonth() + 1
 )
   .toString()
@@ -41,7 +44,7 @@ const MerchantTradeNo = `od${new Date().getFullYear()}${(
   .padStart(2, "0")}${new Date()
   .getSeconds()
   .toString()
-  .padStart(2, "0")}${new Date().getMilliseconds().toString().padStart(2)}`;
+  .padStart(2, "0")}`;
 
 const MerchantTradeDate = new Date().toLocaleDateString("zh-TW", {
   year: "numeric",
@@ -50,102 +53,81 @@ const MerchantTradeDate = new Date().toLocaleDateString("zh-TW", {
   hour: "2-digit",
   minute: "2-digit",
   second: "2-digit",
-  hour12: false
+  hour12: false,
 });
 
-//三、計算 CheckMacValue 之前
-let ParamsBeforeCMV = {
-  ...parameters,
-  "MerchantID": MerchantID,
-  "MerchantTradeNo": MerchantTradeNo,
-  "MerchantTradeDate": MerchantTradeDate.toString(),
-  "PaymentType": "aio",
-  "EncryptType": 1,
-};
-
-//四、計算 CheckMacValue
+//三、、計算 CheckMacValue
 function CheckMacValueGen(parameters, algorithm, digest) {
   const crypto = require("crypto");
-  let Step0;
 
-  Step0 = Object.entries(parameters)
+  const dotNetUrlEncodeMap = {
+    "%2D": "-",
+    "%5F": "_",
+    "%2E": ".",
+    "%21": "!",
+    "%2A": "*",
+    "%28": "(",
+    "%29": ")",
+    "%20": "+",
+  };
+
+  const result = Object.entries(parameters)
     .map(([key, value]) => `${key}=${value}`)
+    .join("&")
+    .split("&")
+    .sort((a, b) => a.split("=")[0].localeCompare(b.split("=")[0]))
     .join("&");
 
-  function DotNETURLEncode(string) {
-    const list = {
-      "%2D": "-",
-      "%5F": "_",
-      "%2E": ".",
-      "%21": "!",
-      "%2A": "*",
-      "%28": "(",
-      "%29": ")",
-      "%20": "+"
-    };
+  let encodedString = encodeURIComponent(
+    `HashKey=${HashKey}&${result}&HashIV=${HashIV}`
+  );
 
-    Object.entries(list).forEach(([encoded, decoded]) => {
-      const regex = new RegExp(encoded, "g");
-      string = string.replace(regex, decoded);
-    });
+  Object.entries(dotNetUrlEncodeMap).forEach(([encoded, decoded]) => {
+    encodedString = encodedString.replace(new RegExp(encoded, "g"), decoded);
+  });
 
-    return string;
-  }
-
-  const Step1 = Step0.split("&")
-    .sort((a, b) => {
-      const keyA = a.split("=")[0];
-      const keyB = b.split("=")[0];
-      return keyA.localeCompare(keyB);
-    })
-    .join("&");
-  const Step2 = `HashKey=${HashKey}&${Step1}&HashIV=${HashIV}`;
-  const Step3 = DotNETURLEncode(encodeURIComponent(Step2));
-  const Step4 = Step3.toLowerCase();
-  const Step5 = crypto.createHash(algorithm).update(Step4).digest(digest);
-  const Step6 = Step5.toUpperCase();
-  return Step6;
+  return crypto
+    .createHash(algorithm)
+    .update(encodedString.toLowerCase())
+    .digest(digest)
+    .toUpperCase();
 }
-const CheckMacValue = CheckMacValueGen(ParamsBeforeCMV, algorithm, digest);
+const CheckMacValue = CheckMacValueGen(getParameters(), "sha256", "hex");
 
-//五、將所有的參數製作成 payload
-const AllParams = {...ParamsBeforeCMV, CheckMacValue};
+//四、將所有的參數製作成 payload
+const AllParams = { ...getParameters(), CheckMacValue };
 const inputs = Object.entries(AllParams)
   .map(function (param) {
     return `<input name=${param[0]} value="${param[1].toString()}"><br/>`;
   })
   .join("");
 
-//六、製作送出畫面
+//五、製作送出畫面
 const htmlContent = `
 <!DOCTYPE html>
 <html>
 <head>
-    <title>全方位金流測試</title>
+    <title>測試</title>
 </head>
 <body>
-    <form method="post" action="${APIURL}">
+    <form id="paymentForm" target="_blank" method="post" action="${APIURL}">
 ${inputs}
-<input type ="submit" value = "送出參數">
+
     </form>
+      <script>
+      window.onload = function() {
+      document.getElementById('paymentForm').submit();
+    };
+  </script>
 </body>
 </html>
 `;
 
-//七、製作出 index.html
-const fs = require("fs");
-
-fs.writeFile("index.html", htmlContent, err => {
-  if (err) {
-    console.error("寫入檔案時發生錯誤:", err);
-  } else {
-    console.log("已建立 index.html");
-    import("open")
-      .then(open => {
-        open.default("index.html");
-      })
-      .catch(error => {
-        console.error("錯誤！", error);
-      });
-  }
+app.get(`/${MerchantTradeNo}`, (req, res) => {
+  res.send(htmlContent);
 });
+
+app.listen(3000, () => {
+  console.log(`http://localhost:3000/${MerchantTradeNo}`);
+});
+ 
--- readme.md
+++ readme.md
@@ -2,20 +2,18 @@
 
 ## 這是什麼？
 
-這是綠界全方位金流的簡單範例，跳轉進入全方位金流的付款頁面。
+這是綠界全方位金流的簡單範例，產生跳轉進入全方位金流付款頁面的連結。
 
 ## 如何使用？
 
 1. 先確定電腦可以執行 Node.js。
-2. 安裝 Node.js 後，請再安裝 open 套件。
-   `npm install open`
-3. 打開本專案的 app.js 檔。
-4. 「一、選擇帳號，是否為測試環境」如果有要更換測試帳號，或改成正式環境(正式帳號請向綠界註冊申請)，才需要更改；否則本段落不需要修改。
-5. 於「二、輸入參數」輸入要修改的參數。由於本示範檔以輸入最少的參數便能進入付款頁面為目的，全方位金流其他更多的參數請看[技術文件](https://developers.ecpay.com.tw/?p=2509)。
-6. 執行 app.js。例如以 VS Code 開啟並按下 F5 再於跳出的選單選擇 Node.js。
-7. 會以預設瀏覽器開啟表格，按下按鈕後跳轉到全方位金流付款頁面。
-8. 完成付款。
-9. 完成付款後，再至[廠商管理後台](https://vendor-stage.ecpay.com.tw/User/LogOn_Step1)查詢該筆訂單。
+2. 打開本專案的 app.js 檔。
+3. 「一、選擇帳號，是否為測試環境」如果有要更換測試帳號，或改成正式環境(正式帳號請向綠界註冊申請)，才需要更改；否則本段落不需要修改。
+4. 於「二、輸入參數」輸入要修改的參數。其中 MerchantTradeNo、 MerchantTradeDate 將自動計算，也可自行設定。由於本示範檔以輸入最少的參數便能進入付款頁面為目的，全方位金流其他更多的參數請看[技術文件](https://developers.ecpay.com.tw/?p=2509)。
+5. 於終端機啟動伺服器 `node app.js`
+6. 點選終端機回傳的網址，進入全方位金流付款頁面。
+7. 完成付款。
+8. 完成付款後，再至[廠商管理後台](https://vendor-stage.ecpay.com.tw/User/LogOn_Step1)查詢該筆訂單。
 
 ## 關於測試帳號與測試信用卡卡號
 
@@ -24,16 +22,15 @@
 
 ## 本範例使用的技術
 
-Node.js(crypto、fs、open)
+Node.js
+
+## 相關參考
+
+[全方位金流建立訂單示範](https://support.ecpay.com.tw/16608/)
 
 ## 本程式作者其他與綠界相關的程式
 
-1. 檢查碼產生器
-   [GitHub](https://github.com/evojroan/checkmacvaluegen)
-   [Medium](https://medium.com/@roan6903/%E7%B6%A0%E7%95%8C%E7%A7%91%E6%8A%80%E6%AA%A2%E6%9F%A5%E7%A2%BC-checkmacvalue-%E7%94%A2%E7%94%9F%E5%99%A8-2fd732a82877)
-2. 電子發票前置作業與資料驗證
-   [GitHub](https://github.com/evojroan/ECPAy_EInvoice_Data_Verification)
-   [Medium](https://medium.com/@roan6903/ecpay-einvoice-data-verification-cbd212cb6d63)
+[綠界技術串接](https://medium.com/@roan6903/list/b0325094c59f)
 
 ## 本程式作者
 
